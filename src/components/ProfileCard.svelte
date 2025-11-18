34b4d4b4c830e5ac1c0284c856f2c7ebf07631d9
kurniawansivan <ivan@wolkk.com>
2025-11-18 18:16:43 +0800

    first commit

---
+++ src/components/ProfileCard.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col items-center w-full max-w-sm rounded-2xl bg-gray-800 p-6 shadow-lg border border-gray-700">
+  
+  <img 
+    src="https://api.dicebear.com/9.x/adventurer/svg?seed=Jocelyn" 
+    alt="User Avatar" 
+    class="mb-4 h-24 w-24 rounded-full border-4 border-gray-700 shadow-sm bg-gray-600"
+  />
+
+  <h2 class="text-xl font-bold text-white">
+    Ivan Kurniawan Sentosa
+  </h2>
+
+  <p class="text-sm font-medium text-gray-400">
+    @backend_wizard
+  </p>
+
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import ProfileCard from "../components/ProfileCard.svelte";
+</script>
+
+<main class="flex h-screen w-screen itens-center justify-center bg-gray-900">
+    <ProfileCard />
+</main>
